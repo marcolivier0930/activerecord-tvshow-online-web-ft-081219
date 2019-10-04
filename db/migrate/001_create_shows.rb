3ec8265adafc2be580f85578f45483b42808b2bd
Marc Blanchard <marcblanchard0930@gmail.com>
2019-10-04 00:55:22 +0000

    Automatically backed up by Learn

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,10 @@
+class CreateShow < ActiveRecords::Migration
+  def change 
+    create_table :shows do |t|
+      t.string :name 
+      t.string :network
+      t.string :day 
+      t.integer :rating 
+    end
+  end
+end
